01286d2cb1f1932697be6ff0a848ddefa64dc41e
Josiah  Partridge <jpartridj94@gmail.com>
2020-04-04 02:51:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index(1) do |dwarf, index|
+    puts "#{index} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |index|
+    index.capitalize() + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_array)
+  if calls_array.length > 4
+    false
+  else
+    true
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array_of_strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array_of_strings.each do |string|
+    if string.include?(cheese_types)
+      string
+    else
+      nil
+    end
+  end
 end
